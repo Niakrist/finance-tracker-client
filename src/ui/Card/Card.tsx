4b2222b96ec1f6fa84258c54f357672231115946
Niakrist <ak_88dron@inbox.ru>
2026-02-20 13:49:27 +0300

    feat: emplement Button and Input

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Icon } from '../Icon'
 import styles from './Card.module.css'
 import type { ICardProps } from './Card.props'
+import cn from 'classnames'
 
-export const Card = ({ children, ...props }: ICardProps) => {
+export const Card = ({
+  children,
+  className,
+  iconName,
+  size,
+  ...props
+}: ICardProps) => {
   return (
-    <div className={styles.card} {...props}>
+    <div
+      className={cn(
+        styles.card,
+        { [styles.small]: size === 'small' },
+        className
+      )}
+      {...props}
+    >
       {children}
+      {iconName && <Icon name={iconName} />}
     </div>
   )
 }
--- src/ui/Icon/Icon.tsx
+++ src/ui/Icon/Icon.tsx
@@ -5,8 +5,11 @@ import { IconAnalytics } from './icons/IconAnalytics'
 import { IconCategories } from './icons/IconCategories'
 import { IconTransactions } from './icons/IconTransactions'
 import { IconSettings } from './icons/IconSettings'
+import { IconPaid } from './icons/IconPaid'
+import IconArrowDown from './icons/IconArrowDown'
+import IconArrowUp from './icons/IconArrowUp'
 
-interface IIconProps extends React.SVGProps<SVGSVGElement> {
+export interface IIconProps extends React.SVGProps<SVGSVGElement> {
   name:
     | 'iconNotification'
     | 'iconDashboard'
@@ -14,6 +17,9 @@ interface IIconProps extends React.SVGProps<SVGSVGElement> {
     | 'iconCategories'
     | 'iconTransactions'
     | 'iconSettings'
+    | 'iconPaid'
+    | 'iconArrowDown'
+    | 'iconArrowUp'
   className?: string
 }
 
@@ -24,7 +30,10 @@ export const Icon = ({ name, className, ...props }: IIconProps) => {
     iconAnalytics: <IconAnalytics className={className} {...props} />,
     iconCategories: <IconCategories className={className} {...props} />,
     iconTransactions: <IconTransactions className={className} {...props} />,
-    iconSettings: <IconSettings />
+    iconSettings: <IconSettings className={className} {...props} />,
+    iconArrowDown: <IconArrowDown className={className} {...props} />,
+    iconArrowUp: <IconArrowUp className={className} {...props} />,
+    iconPaid: <IconPaid {...props} />
   }
   return icons[name]
 }
